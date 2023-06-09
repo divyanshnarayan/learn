31aa90e74195834033ffee80a4432d49e49e6a29
unknown <dpsdiv07263@gmail.com>
2023-06-09 12:44:31 +0530

    Template for CF codes

---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define vs vector<string>
+#define vi_i vector<vector<int>>
+#define vll_ll vector<vector<long long int>>
+#define vpii vector<pair<int,int>>
+#define vpll vector<pair<long long int,long long int>>
+#define fu(i,l,n) for(int i=l;i<=n;i++)
+#define fd(i,n,l) for(int i=n;i>=l;i--)
+#define modulo 1000000007
+int main(){
+int t;
+cin>>t;
+while(t--)
+{
+    
+}
+}
